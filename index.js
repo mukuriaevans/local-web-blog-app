a71100235ee31732484f31129d52aa075cef4ad3
mukuriaevans <33530066+mukuriaevans@users.noreply.github.com>
2024-02-27 01:12:37 +0300

    update index.js and add compose.ejs

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 import express from "express";
 import bodyParser from "body-parser";
+import localStorage from "localStorage";
 
 const app = express();
 const port = 3000;
+const blogs = [];
 
 app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
+// serve the home page
+app.get("/", (req, res) => {
+    
+    res.render("index", {blogs: blogs});
+});
 
+// handle the get compose request
+app.get('/compose', (req, res) => {
+    res.render('compose');
+})
+
+// handle the post resquest
+app.post("/submit", (req, res) => {
+   const blogPost = {
+    postTitle : req.body["pTitle"],
+    postBody: req.body["pBody"]
+   }
+//    app new blogpost to the blogs array
+    blogs.push(blogPost);
+
+    res.redirect('/');
+
+    console.log(blogs);
+
+})
 
 
 
